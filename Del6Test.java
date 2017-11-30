9e9e30b5378a7646f917ce8a65e6f837e95365d9
Branden Keck <bmkeck62@gmail.com>
2017-11-30 08:29:33 -0500

    Init

---
+++ Del6Test.java
@@ -0,0 +1,32 @@
+//Java imports
+import java.lang.*;
+import java.util.ArrayList;
+
+//JUnit and Mockito imports
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * This is the test class for Deliverable 5, CS1632 Fall 2017
+ * Authors: Randyll Bearer (github: rlb97) and Branden Keck (github: BrandenKeck)
+ *
+ */
+
+public class Del5Test {
+	//Declare reusable element object
+	/* OBJECT CREATION HERE*/
+	
+	// Create a new Element instance before testing
+    @Before
+    public void setup() {
+		/*INITIALIZE OBJECT*/
+    }
+    
+	@Test
+	public void test1() {
+		
+		
+	}
+	
+}
