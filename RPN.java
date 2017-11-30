35f39638c5469442664f62d289f2e93bdd5d04c7
Branden Keck <bmkeck62@gmail.com>
2017-11-30 08:45:48 -0500

    Basic SetUp

---
+++ RPN.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+Stack stack = new Stack();
+
+public static void readFiles(){
+	
+	
+}
+
+public static void runLoop(){
+	
+	
+}
+
+
+public static void main(String[] args) {
+	if(args.length==0){
+		runLoop();
+	}else{
+		readFiles();
+	}
+}
