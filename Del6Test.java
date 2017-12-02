d1fb00dd26e4ad0e7a2962870c5f9649c3bc1ff6
rlb97 <rlb97@pitt.edu>
2017-12-02 11:31:09 -0500

    Merged Both Branches
    Merged BrandenKeck's code with my own, making sure to split it into
    methods which are testable
    
    Began writing unit tests
    
    Ran findbugs and fixed all issues
    
    Ran styleChecker and fixed all except missing javadocComment.

--- Del6Test.java
+++ Del6Test.java
@@ -1,6 +1,7 @@
 //Java imports
 import java.lang.*;
 import java.util.ArrayList;
+import java.io.*;
 
 //JUnit and Mockito imports
 import org.junit.*;
@@ -14,20 +15,75 @@ import org.mockito.*;
  *
  */
 
-public class Del5Test {
-	//Declare reusable element object
-	/* OBJECT CREATION HERE*/
+public class Del6Test {
+	//global
+	RPN testRPN = null;
+	InputStream standardIn = System.in;
+	PrintStream standardOut = System.out;
 	
 	// Create a new Element instance before testing
     @Before
     public void setup() {
-		/*INITIALIZE OBJECT*/
+		RPN testRPN = new RPN();
+		System.setIn(standardIn);
+		System.setOut(standardOut);
     }
-    
+	
+	//
+	@After
+	public void tearDown() {
+		System.setIn(standardIn);
+		System.setOut(standardOut);
+	}
+	
+	//------------ runLoop() ------------
+	//
 	@Test
-	public void test1() {
+	public void testValidVariableInitialization1() {
+		ByteArrayInputStream testIn = new ByteArrayInputStream("Let a 10 20 +".getBytes());
+		ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+		System.setIn(testIn);
+		System.setOut(new PrintStream(testOut));
 		
+		ArrayList<String> testOutput = testRPN.debugRunLoop(1);
+		
+		String expectedOutput = "30";
+		String observedOutput = testOutput.get(0);
+
+		assertEquals(expectedOutput, observedOutput );
 		
 	}
 	
+	//------------- read() ---------------
+	//
+	
+	@Test
+	public void test2() {
+		
+		
+		assertTrue(true);
+	}
+	
+	/*
+	//------------ readFiles() ----------
+	//
+	@Test
+	public void test0() {
+		
+	}
+	
+	//------------- eval() ----------------
+	//
+	@Test
+	public void test3() {
+		
+	}
+	
+	//------------- print() ---------------
+	//
+	@Test
+	public void test4() {
+		
+	}
+	*/
 }
--- RPN.java
+++ RPN.java
@@ -1,178 +1,266 @@
-import java.util.*;
-import java.lang.*;
-import java.math.BigInteger;
+import java.math.BigInteger;	//https://docs.oracle.com/javase/7/docs/api/java/math/BigInteger.html
+import java.util.*;	
 
+/*
+*
+*/
 public class RPN {
+	//https://docs.oracle.com/javase/7/docs/api/java/util/HashMap.html
+	private static HashMap<String, BigInteger> variables = new HashMap<String, BigInteger>();	
+	private static int lineNum = 0;
+	private static boolean errorFlag = false;
 	
-	static HashMap<String, BigInteger> variables = new HashMap<String, BigInteger>();
+	//------------ functional methods ----------------
 	
-	public static void readFiles(){
+	/*
+	* Concatenate all file paths into 1 string
+	* read() / eval() each string
+	* Mode = 0
+	*/
+	public static void readFiles() {
 		
 		
 	}
 
-	public static void runLoop(){
-		Scanner scanMe = new Scanner(System.in);
-		BigInteger res;
-		int lineNumber = 0;
-		
-		while(true){
-			System.out.print("\n> ");
-			String input = scanMe.nextLine().toUpperCase();
-			String[] items = input.split(" ");
-			lineNumber++;
+	/*
+	* Read(), Eval(), Print()
+	*/
+	public static void runLoop() {
+		//will have to have eval() consume a 'QUIT' keyword to break this loop
+		while (true) {
 			
-			if(items.length>0){
-				handleKeywords(items, lineNumber);
+			//https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
+			String[] resultTokens = read(1);
+			
+			String resultString = "";
+			if (resultTokens.length > 0 ) {
+				resultString = eval(resultTokens, 1);
 			}
+			if (resultString.equals("QUIT")) {
+				break;
+			}
+			
+			print(resultString, 1);
+				
 		}
 	}
-	
-	public static void handleKeywords(String[] items, int lN){
-		BigInteger t, res;
-		int l = items.length;
+
+	/*
+	* Reads in a line of input from the user
+	* Splits it by a whitespace delimiter
+	* returns the array of split strings
+	*/
+	public static String[] read(int mode) {
+		//variables
+		Scanner scanner = new Scanner(System.in, "UTF-8");
+		String[] tokens = null;
 		
-		switch(items[0]){
-			case "LET":
-				if(items[1].length()!=1){
-					System.out.println("Line "+Integer.toString(lN)+": Unknown keyword "+items[1]);
-					System.out.println("Terminating Operation...");
-				}else{
-					char c = items[1].charAt(0);
-					int ascii = (int)c;
-					if(ascii>=65&&ascii<=90){
-						String tryVar = items[1];
-						if(l == 3){
-							try{
-								t = new BigInteger(items[2]);
-								variables.put(tryVar, t);
-								System.out.println(""+t);
-							}catch(NumberFormatException e2){
-								if(variables.get(items[2]) != null){
-									t = variables.get(items[2]);
-									variables.put(tryVar, t);
-									System.out.println(""+t);
-								}else{
-									System.out.println("Line "+Integer.toString(lN)+": Variable "+items[2]+" is not initialized");
-									System.out.println("Terminating Operation...");
-								}
-							}
-						}else{
-							String[] calcArray = Arrays.copyOfRange(items, 2, l);
-							res = calculate(calcArray, lN);
-							if(res != null){
-								variables.put(tryVar, res);
-								System.out.println(""+res);	
-							}
-						}
-					}else{
-						System.out.println("Line "+Integer.toString(lN)+": Variable name "+items[1]+" is not valid.\nValid variable names are single characters a-z.");
-						System.out.println("Terminating Operation...");
-					}
-				}
-				break;
-			case "PRINT":
-				if(l == 2){
-					try{
-						t = new BigInteger(items[1]);
-						System.out.println(""+t);
-					}catch(NumberFormatException e2){
-						if(variables.get(items[1]) != null){
-							t = variables.get(items[1]);
-							System.out.println(""+t);
-						}else{
-							System.out.println("Line "+Integer.toString(lN)+": Variable "+items[1]+" is not initialized");
-							System.out.println("Terminating Operation...");
-						}
-					}
-				}else{
-					String[] calcArray = Arrays.copyOfRange(items, 1, l);
-					res = calculate(calcArray, lN);
-					if(res != null){
-						System.out.println(""+res);	
-					}
-				}
-				break;
-			case "QUIT":
-				System.exit(0);
-				break;
-			default:
-				res = calculate(items, lN);
-				if(res != null){
-					System.out.println(""+res);	
-				}
-				break;
-		}
+		//logic
+		System.out.print("> ");
+		String userString = scanner.nextLine();
+		userString = userString.toUpperCase(Locale.ENGLISH);	//allow for case-insenstivity
+		tokens = userString.split(" ");
+		
+		//return
+		//find some way to close() scanner without closing system.in()?
+		lineNum = lineNum + 1;
+		return tokens;
 	}
-	
-	public static BigInteger calculate(String[] items, int lineNumber){
+
+	/*
+	* Accepts an array of split strings, interpret one at a time
+	* if num, push to stack / variable, act accordingly / keyword, act accordingly
+	* return the String of what should be printed
+	*/
+	public static String eval(String[] toEval, int mode) {
+		//variables
+		String result = "";
 		Stack stack = new Stack();
-		BigInteger t, t1, t2;
-		String[] acceptable = {"+", "-", "*", "/"};
+		String currentToken = "";
+		BigInteger number = null;
+		boolean letFlag = false;
+		String toChange = "";
+		errorFlag = false;
 		
-		if(items.length>0){
-			for(int i=0; i<items.length; i++){
-				try{
-					t = new BigInteger(items[i]);
-					stack.push(t);
-				}catch(NumberFormatException e){
-					if(Arrays.asList(acceptable).contains(items[i])){
-						if(stack.size()>1){
-							switch(items[i]){
-								case "+":
-									t1 = (BigInteger) stack.pop();
-									t2 = (BigInteger) stack.pop();
-									stack.push(t2.add(t1));
-									break;
-								case "-":
-									t1 = (BigInteger) stack.pop();
-									t2 = (BigInteger) stack.pop();
-									stack.push(t2.subtract(t1));
-									break;
-								case "*":
-									t1 = (BigInteger) stack.pop();
-									t2 = (BigInteger) stack.pop();
-									stack.push(t2.multiply(t1));
-									break;
-								case "/":
-									t1 = (BigInteger) stack.pop();
-									t2 = (BigInteger) stack.pop();
-									stack.push(t2.divide(t1));
-									break;
-							}
-						}else{
-							System.out.println("Line "+Integer.toString(lineNumber)+": Operator "+items[i]+" applied to empty stack");
-							System.out.println("Terminating Operation...");
-							return null;
-						}
-					}else{
-						if(variables.get(items[i])!=null){
-							t = variables.get(items[i]);
-							stack.push(t);
-						}else{
-							System.out.println("Line "+Integer.toString(lineNumber)+": Variable "+items[i]+" is not initialized");
-							System.out.println("Terminating Operation...");
-							return null;
-						}
+		//logic
+		int i = 0;
+		while (i < toEval.length) {
+			currentToken = toEval[i];
+		
+			try {
+				number = new BigInteger(toEval[i]);
+				stack.push(number);
+				
+			} catch (NumberFormatException nfe) {
+				int toCheck = (int)currentToken.charAt(0);
+				
+				if (currentToken.equals("LET") && i == 0) {					
+					//let must be followed by variable + values
+					i = i + 1;
+					if (i + 1 >= toEval.length) {
+						result = "Line " + lineNum + ": Operator LET applied to Empty stack";
+						errorFlag = true;
+						return result;
 					}
+					
+					toChange = toEval[i];
+					
+					if (toChange.length() != 1 || ( toCheck < 65 || toCheck > 90 ) ) {
+						result = "Line " + lineNum + ": Operator LET applied to empty stack";
+						errorFlag = true;
+						return result;
+					}
+					letFlag = true;
+					
+				} else if (currentToken.equals("PRINT") && i == 0 ) {
+					//do nothing
+					
+				} else if (currentToken.equals("QUIT") && i == 0 ) {
+					return "QUIT";
+					
+				} else if (currentToken.equals("+") ) {
+					try {
+						BigInteger second = (BigInteger)stack.pop();
+						BigInteger first = (BigInteger)stack.pop();
+						stack.push(first.add(second) );
+					} catch (EmptyStackException ese) {
+						result = "Line " + lineNum + ": Operator + applied to empty stack";
+						errorFlag = true;
+						return result;
+					}
+					
+				} else if (currentToken.equals("-") ) {
+					try {
+						BigInteger second = (BigInteger)stack.pop();
+						BigInteger first = (BigInteger)stack.pop();
+						stack.push(first.subtract(second) );
+					} catch (EmptyStackException ese) {
+						result = "Line " + lineNum + ": Operator - applied to empty stack";
+						errorFlag = true;
+						return result;
+					}
+					
+				} else if (currentToken.equals("/") ) {
+					try {
+						BigInteger second = (BigInteger)stack.pop();
+						BigInteger first = (BigInteger)stack.pop();
+						stack.push(first.divide(second) );
+					} catch (EmptyStackException ese) {
+						result = "Line " + lineNum + ": Operator / applied to empty stack";
+						errorFlag = true;
+						return result;
+					}
+					
+				} else if (currentToken.equals("*") ) {
+					try {
+						BigInteger second = (BigInteger)stack.pop();
+						BigInteger first = (BigInteger)stack.pop();
+						stack.push(first.multiply(second) );
+					} catch (EmptyStackException ese) {
+						result = "Line " + lineNum + ": Operator * applied to empty stack";
+						errorFlag = true;
+						return result;
+					}
+					
+				} else if ( currentToken.length() == 1 && toCheck > 64 && toCheck < 91 ) {
+					BigInteger variable = variables.get(currentToken);
+					if (variable == null) {
+						result = "Line " + lineNum + ": Variable '";
+						result = result + currentToken + "' is not initialized";
+						errorFlag = true;
+						return result;
+					}
+					stack.push(variable);
+					
+				} else {
+					result = "Line " + lineNum + ": Unknown keyword " + currentToken;
+					errorFlag = true;
+					return result;
 				}
-				if(i==(items.length-1) && stack.size()>1){
-					String output = Integer.toString(stack.size()) + " elements in stack after evaluation";
-					System.out.println("Line "+Integer.toString(lineNumber)+": "+output);
-					return null;
-				}
+				
 			}
+			
+			
+			i = i + 1;
 		}
 		
-		return (BigInteger) stack.pop();
+		//return
+		if (stack.size() > 1) {
+			result = "Line " + lineNum + ": " + stack.size() + " elements in stack after evaluation";
+			errorFlag = true;
+			return result;
+		}
+		
+		BigInteger resultant = (BigInteger)stack.pop();
+		
+		//write to variable
+		if (letFlag == true) {
+			variables.put(toChange, resultant);
+		}
+		
+		result = resultant.toString();
+		return result;
 	}
 
+	/*
+	* Accepts a string and prints it back to user
+	*/
+	public static void print(String toPrint, int mode) {
+		if (errorFlag == true) {
+			System.err.println(toPrint);
+		} else {
+			System.out.println(toPrint);
+		}
+		
+	}
 
+	/*
+	* Main method, act accordingly
+	*/
 	public static void main(String[] args) {
-		if(args.length==0){
+		
+		if (args.length == 0) {
 			runLoop();
-		}else{
+		} else { 
 			readFiles();
 		}
+		
+		System.exit(0);
+		
 	}
+	
+	//------------ debug code ---------------------
+	private static ArrayList<String> debugOutput = new ArrayList<String>();
+	
+	/*
+	* To be used as a way to test the runLoop() function
+	*/
+	public static ArrayList<String> debugRunLoop(int i) {
+		//will have to have eval() consume a 'QUIT' keyword to break this loop'
+		int j = 0;
+		while (j < i) {
+			
+			//https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
+			String[] resultTokens = read(1);
+			
+			String resultString = "";
+			if (resultTokens.length > 0) {
+				resultString = eval(resultTokens, 1);
+			}
+			if (resultString.equals("QUIT")) {
+				break;
+			}
+			debugOutput.add(resultString);
+			
+			print(resultString, 1);
+			
+			j = j + 1;
+		}
+		
+		return debugOutput;
+		
+	}
+	
+	//end of program
 }
+
